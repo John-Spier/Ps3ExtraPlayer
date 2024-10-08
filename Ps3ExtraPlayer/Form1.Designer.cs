d667069d16975b7484ec997658bc7484dfca9b99
John-Spier <102952652+John-Spier@users.noreply.github.com>
2024-10-07 23:30:56 -0400

    Add project files.

---
+++ Ps3ExtraPlayer/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Ps3ExtraPlayer
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			listView1 = new ListView();
+			folderBrowserDialog1 = new FolderBrowserDialog();
+			SuspendLayout();
+			// 
+			// listView1
+			// 
+			listView1.Activation = ItemActivation.OneClick;
+			listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			listView1.HotTracking = true;
+			listView1.HoverSelection = true;
+			listView1.Location = new Point(12, 12);
+			listView1.MultiSelect = false;
+			listView1.Name = "listView1";
+			listView1.Size = new Size(776, 426);
+			listView1.TabIndex = 0;
+			listView1.UseCompatibleStateImageBehavior = false;
+			listView1.View = View.List;
+			listView1.ItemActivate += listView1_ItemActivate;
+			listView1.DragDrop += listView1_DragDrop;
+			listView1.DragEnter += listView1_DragEnter;
+			listView1.KeyPress += listView1_KeyPress;
+			listView1.MouseDoubleClick += listView1_MouseDoubleClick;
+			// 
+			// Form1
+			// 
+			AllowDrop = true;
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(listView1);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			Name = "Form1";
+			Text = "PS3 Extra Player";
+			Click += Form1_Click;
+			DragDrop += Form1_DragDrop;
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private ListView listView1;
+		private FolderBrowserDialog folderBrowserDialog1;
+	}
+}
---
+++ Ps3ExtraPlayer/Form1.cs
@@ -0,0 +1,111 @@
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Ps3ExtraPlayer
+{
+	public partial class Form1 : Form
+	{
+		public Dictionary<string, string>? Dictionary { get; set; }
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void listView1_DragDrop(object sender, DragEventArgs e)
+		{
+			//why doesnt this work
+		}
+
+		string ParseOneFile(string XmlPath)
+		{
+			XAttribute SfxKey = new("key", "TITLE");
+			XDocument ParamSfx = XDocument.Load(XmlPath);
+			XElement ParamSfo = ParamSfx.Element("paramsfo");
+			IEnumerable<XElement> ParamTitle =
+				from x in ParamSfo.Elements()
+				where x.Attribute("key").Value == "TITLE"
+				select x;
+			return ParamTitle.FirstOrDefault().Value;
+		}
+
+		private void listView1_DragEnter(object sender, DragEventArgs e)
+		{
+			//MessageBox.Show(e.Effect.ToString());
+			//e.Effect = DragDropEffects.All;
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e)
+		{
+			//MessageBox.Show("Form Dropped");
+		}
+
+		private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+
+		}
+
+		private void Form1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void LoadFilesFromDialog()
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string fpath = folderBrowserDialog1.SelectedPath;
+				Dictionary ??= [];
+				string p;
+
+				try
+				{
+					foreach (string d in Directory.GetFiles(fpath, "PARAM.SFX", SearchOption.AllDirectories))
+					{
+						p = ParseOneFile(d);
+						Dictionary.Add(p, Path.GetDirectoryName(d) + "\\DATA000" + Path.GetExtension(p));
+						listView1.Items.Add(p);
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+		}
+
+		private void listView1_ItemActivate(object sender, EventArgs e)
+		{
+			try
+			{
+				new Process
+				{
+					StartInfo = new ProcessStartInfo(Dictionary[listView1.SelectedItems[0].Text])
+					{
+						UseShellExecute = true
+					}
+				}.Start();
+			}
+			catch (Exception f)
+			{
+				MessageBox.Show(f.Message);
+			}
+		}
+
+		private void listView1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!e.Handled) 
+			{
+				switch (e.KeyChar)
+				{
+					case (char)27:
+						listView1.Items.Clear();
+						LoadFilesFromDialog();
+						break;
+					default:
+						break;
+				}
+
+			}
+		}
+	}
+}
